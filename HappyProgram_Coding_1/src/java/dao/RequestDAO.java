f21ce70f3c84d8ea21167eab1bc98ff893549f85
Nguyenthienan3006 <92444045+Nguyenthienan3006@users.noreply.github.com>
2023-06-19 13:09:43 +0700

    an
    thêm hàm updateDAO req status

--- HappyProgram_Coding_1/src/java/dao/RequestDAO.java
+++ HappyProgram_Coding_1/src/java/dao/RequestDAO.java
@@ -1,7 +1,36 @@
 package dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import model.*;
 
-public class RequestDAO extends MyDAO{
-    
+public class RequestDAO extends MyDAO {
+
+    public int UpdateRequest_Mentor(int requestID) {
+        int mess = 0;
+        // Chuỗi truy vấn SELECT để lấy danh sách mentor
+        String query = "UPDATE Request\n"
+                + "SET RequestStatus = 2\n"
+                + "WHERE RequestID = ?;";
+        try {
+            ps = con.prepareStatement(query);
+            ps.setInt(1, requestID);
+            // Đóng kết nối và giải phóng tài nguyên
+            
+            ps.executeUpdate();
+            ps.close();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            mess = 1;
+        }
+        return mess;
+    }
+
+    public static void main(String[] args) {
+        RequestDAO r = new RequestDAO();
+        r.UpdateRequest_Mentor(3);
+    }
 }
+
